2e5302cacb6013a2bff083b14fa0e12a9b7624e6
hamster-0914 <kexintankx@gmail.com>
2023-11-15 12:18:02 +0800

    Revert "new update"
    This reverts commit 649d05ccd757bdc33a5788cc5dacbc0e50de453a, reversing
    changes made to 1bbe40031018a6c0eb7ed44ab96692c3d89080e0.

--- DDAC Assignment/Program.cs
+++ DDAC Assignment/Program.cs
@@ -1,20 +1,7 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using DDAC_Assignment.Data;
-using DDAC_Assignment.Areas.Identity.Data;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Builder;
-
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("DDAC_AssignmentContextConnection") ?? throw new InvalidOperationException("Connection string 'DDAC_AssignmentContextConnection' not found.");
-
-builder.Services.AddDbContext<DDAC_AssignmentContext>(options => options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<DDAC_AssignmentUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<DDAC_AssignmentContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
@@ -30,11 +17,11 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+
 app.Run();
